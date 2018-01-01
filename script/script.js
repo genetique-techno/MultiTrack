82962c30230b21d06f5de7984e71e011c5673026
JeremyLaptop <nital101@yahoo.com>
2018-01-01 18:08:47 -0500

    add code for time formatting and ui function

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ class TaskList {
 
 	addTask() {
 
-		let newTask = new Task(prompt());
+		let newTask = new Task(document.getElementById('name-input').value);
 		this.tasks.push(newTask);
 	}
 };
@@ -26,9 +26,24 @@ class Task {
     	} else {
     		this._status = "PAUSED";
     		this._time = (this._time + Date.now() - this._startTime);
-    	}
+    	};
+			const timeInSeconds = (this._time/1000);
+			function timeFormat(){
+				const roundedTime = Math.round(timeInSeconds);
+				let hours = Math.floor(roundedTime / 3600);
+				let mins = Math.floor((roundedTime - (hours * 3600)) / 60);
+				let secs = roundedTime - (hours * 3600) - (mins * 60);
+				if ((hours === 0) && (mins === 0)){
+				    return secs + 's ';
+				}else if(hours === 0) {
+				    return mins + 'm ' + secs + 's ';
+				}else{
+				    return hours + 'h ' + mins + 'm ' + secs + 's ';
+			  }
+			};
     	console.log(this._name, this._status, (this._time/1000));
-      document.getElementById('text').innerHTML = this.name + ' ' + this.status + ' ' + this.time;
+      document.getElementById('task-status').innerHTML = this.status;
+			document.getElementById('task-time').innerHTML = timeFormat();
     }
 
     get name() {
@@ -44,4 +59,12 @@ class Task {
     }
 };
 
-const main = new TaskList;
+	const main = new TaskList;
+	function submitInput() {
+		document.getElementById('name-input').disabled = true;
+		document.getElementById('submit-name').remove();
+		document.getElementById('run-pause').style.visibility = 'visible';
+	}
+  document.getElementById('submit-name').addEventListener("click", function(){main.addTask()});
+	document.getElementById('submit-name').addEventListener("click", function(){submitInput()});
+	document.getElementById('run-pause').addEventListener("click", function(){main.tasks[0].runStop()});
