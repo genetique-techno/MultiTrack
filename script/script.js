6b08908757bdbec43f7b413159c8a6e313280a21
Jeremy <nital101@yahoo.com>
2018-01-03 21:42:42 -0500

    work on new task button HTML only

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//parent container for tasks
 class TaskList {
 	constructor() {
     this.tasks = [];
@@ -10,6 +11,7 @@ class TaskList {
 	}
 };
 
+//task template
 class Task {
     constructor(name) {
         this._name = name;
@@ -27,6 +29,7 @@ class Task {
     		this._status = "PAUSED";
     		this._time = (this._time + Date.now() - this._startTime);
     	};
+			//display time in hrs/mins/seconds, calculated each time play/pause button is clicked
 			const timeInSeconds = (this._time/1000);
 			function timeFormat(){
 				const roundedTime = Math.round(timeInSeconds);
@@ -59,12 +62,36 @@ class Task {
     }
 };
 
+function createNewTaskRow(){
+	//instance of task
+	let taskInstance = 0;
+	//define all HTML elements in task row
+	let taskRow = document.createElement('div');
+	let input = document.createElement('input');
+	let submitButton = document.createElement('button');
+	let playPauseButton = document.createElement('button');
+	let taskStatus = document.createElement('p');
+	let taskTime = document.createElement('p');
+
+	input.type = 'text';
+
+	document.getElementById('tasklist').appendChild(taskRow);
+	taskRow.appendChild(input);
+  taskRow.appendChild(submitButton);
+	taskRow.appendChild(playPauseButton);
+	taskRow.appendChild(taskStatus);
+	taskRow.appendChild(taskTime);
+
+}
+
 	const main = new TaskList;
 	function submitInput() {
 		document.getElementById('name-input').disabled = true;
 		document.getElementById('submit-name').remove();
 		document.getElementById('run-pause').style.visibility = 'visible';
 	}
+
   document.getElementById('submit-name').addEventListener("click", function(){main.addTask()});
 	document.getElementById('submit-name').addEventListener("click", function(){submitInput()});
+		document.getElementById('new-task').addEventListener("click", function(){createNewTaskRow()});
 	document.getElementById('run-pause').addEventListener("click", function(){main.tasks[0].runStop()});
