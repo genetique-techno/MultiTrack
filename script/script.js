c050eadcf09a5adef951b1d1efd2a42a8f8e5e11
Jeremy <nital101@yahoo.com>
2018-01-08 19:43:17 -0500

    add function to dynamically create html elements

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-//parent container for tasks
+
+//Parent container for tasks
 class TaskList {
 	constructor() {
     this.tasks = [];
@@ -6,12 +7,12 @@ class TaskList {
 
 	addTask() {
 
-		let newTask = new Task(document.getElementById('name-input').value);
+		let newTask = new Task(inputText[taskFocus].value);
 		this.tasks.push(newTask);
 	}
 };
 
-//task template
+//Task template
 class Task {
     constructor(name) {
         this._name = name;
@@ -20,7 +21,7 @@ class Task {
         this._startTime = undefined;
     }
 
-    //play or pause button
+    //Play or pause button
     runStop() {
       if (this._status === "PAUSED"){
     		this._status = "RUNNING";
@@ -29,7 +30,7 @@ class Task {
     		this._status = "PAUSED";
     		this._time = (this._time + Date.now() - this._startTime);
     	};
-			//display time in hrs/mins/seconds, calculated each time play/pause button is clicked
+			//Display time in hrs/mins/seconds, calculated each time play/pause button is clicked
 			const timeInSeconds = (this._time/1000);
 			function timeFormat(){
 				const roundedTime = Math.round(timeInSeconds);
@@ -62,36 +63,86 @@ class Task {
     }
 };
 
+//Instance of task to be created.
+let nextTaskInstance = 0;
+
+let submitListener = undefined;
+
 function createNewTaskRow(){
-	//instance of task
-	let taskInstance = 0;
-	//define all HTML elements in task row
+
+	//Define all HTML elements in task row
 	let taskRow = document.createElement('div');
 	let input = document.createElement('input');
 	let submitButton = document.createElement('button');
-	let playPauseButton = document.createElement('button');
+	let runStopButton = document.createElement('button');
 	let taskStatus = document.createElement('p');
 	let taskTime = document.createElement('p');
 
+	//Define attributes
 	input.type = 'text';
+	input.value = 'Enter Name Here';
+	input.className = 'task' + nextTaskInstance + ' input';
+	submitButton.innerHTML = 'SUBMIT';
+	submitButton.className = 'task' + nextTaskInstance + ' submit';
+	runStopButton.innerHTML = 'RUN/STOP';
+	runStopButton.className = 'task' + nextTaskInstance + ' run-stop';
+	taskStatus.className = 'task' + nextTaskInstance + ' status';
+	taskTime.className = 'task' + nextTaskInstance + ' time';
 
+	//Build taskRow
 	document.getElementById('tasklist').appendChild(taskRow);
 	taskRow.appendChild(input);
   taskRow.appendChild(submitButton);
-	taskRow.appendChild(playPauseButton);
+	taskRow.appendChild(runStopButton);
 	taskRow.appendChild(taskStatus);
 	taskRow.appendChild(taskTime);
 
+	//Attach event listeners for each button
+	submitListener = document.getElementsByClassName('submit');
+	runStopListener = document.getElementsByClassName('run-stop');
+
+  for (let i = 0; i < submitListener.length; i++) {
+		submitListener[i].addEventListener('click', function(){
+			taskFocus = i;
+			console.log('Task ' + taskFocus + ' is in focus. Task name is: ' + inputText[taskFocus].value);
+		});
+	};
+
+	for (let i = 0; i < runStopListener.length; i++) {
+		runStopListener[i].addEventListener('click', function(){
+			taskFocus = i;
+			console.log('Task ' + taskFocus + ' is in focus. Task name is: ' + inputText[taskFocus].value);
+		});
+	};
+
+	//Increment for next task
+	nextTaskInstance ++
+};
+
+//New task button
+document.getElementById('new-task').addEventListener("click", function(){createNewTaskRow()});
+
+//Initialize tasklist and taskrow on page load.
+createNewTaskRow();
+const main = new TaskList;
+
+
+
+
+//Select the focus of a task when a task button is clicked
+let taskFocus = 0;
+let inputText = document.getElementsByClassName('input')
+
+
+
+
+
+//Example of standalone operation
+function submitInput() {
+	document.getElementById('name-input').disabled = true;
+	document.getElementById('submit-name').remove();
+	document.getElementById('run-pause').style.visibility = 'visible';
 }
-
-	const main = new TaskList;
-	function submitInput() {
-		document.getElementById('name-input').disabled = true;
-		document.getElementById('submit-name').remove();
-		document.getElementById('run-pause').style.visibility = 'visible';
-	}
-
-  document.getElementById('submit-name').addEventListener("click", function(){main.addTask()});
+  document.getElementById('submit-name').addEventListener("click", function(){console.log(main.tasks)});
 	document.getElementById('submit-name').addEventListener("click", function(){submitInput()});
-		document.getElementById('new-task').addEventListener("click", function(){createNewTaskRow()});
 	document.getElementById('run-pause').addEventListener("click", function(){main.tasks[0].runStop()});
